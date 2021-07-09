dcec1aec780fff15f79a0042e9dae53a8648598b
abhishek6844 <71318680+abhishek6844@users.noreply.github.com>
2021-07-09 19:30:30 +0530

    Add files via upload

---
+++ pythonvariables.py
@@ -0,0 +1,26 @@
+
+#arithmatic operators +, -, \, \\, %=modulus
+x = 25
+x +=10
+print(x)
+
+#
+x =1
+y= 7
+print(x**y)
+
+#comparison operator 
+a = 6
+b = 6
+print(a==b)
+print(a>b)
+print(a<b)
+
+#and operator
+print(x>5 and y<7)
+
+# or operator
+print(x>5 or x<6)
+
+#not
+print(not(x>7 or y<2)) 
